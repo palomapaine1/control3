8a7a0a4461c5f10bd632ccbaf222b402f759734d
palomapaine1 <palomapaine4@gmail.com>
2024-11-08 17:09:53 -0300

    Update untitled7(3).py

--- untitled7(3).py
+++ untitled7(3).py
@@ -15,7 +15,7 @@ st.litle('uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+         ('streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache
 def load_data(nrows):
